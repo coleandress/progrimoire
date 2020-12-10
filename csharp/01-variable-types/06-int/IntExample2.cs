60d17839894223c56555d6967791e5d7618f3842
Cole Andress <coleandress@gmail.com>
2020-12-10 12:19:18 -0600

    Finished C# int examples.

---
+++ csharp/01-variable-types/06-int/IntExample2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class IntExample2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"The size of an int is: {sizeof(int)} bytes\n");
+
+            var maxValue = 0;
+
+            for (int i = 0, n = sizeof(int); i < n; i++)
+                maxValue |= (i + 1 == n ? 0x7f : 0xff) << (i * 8);
+
+            var theInteger = maxValue;
+            
+            Console.WriteLine($"theInteger: {theInteger} (hex: {theInteger:x8})\n");
+
+            theInteger -= 147483647;
+            
+            Console.WriteLine($"theInteger: {theInteger} (hex: {theInteger:x8})\n");
+
+            theInteger = -maxValue - 1;
+            
+            Console.WriteLine($"theInteger: {theInteger}\n");
+            
+            /* -- This will underflow as we're already at the
+                  min allowed value for an integer -- */
+
+            theInteger = theInteger - 1;
+            
+            Console.WriteLine($"theInteger: {theInteger}\n");
+
+            theInteger = maxValue;
+            
+            /* -- This will overflow as we're already at
+                  the max allowed value for an integer -- */
+
+            theInteger = theInteger + 1;
+            
+            Console.WriteLine($"theInteger: {theInteger}\n");
+        }
+    }
+}
---
+++ csharp/01-variable-types/06-int/IntExample3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class IntExample3
+    {
+        public static void Main(string[] args)
+        {
+            Int32 theIntOne  = 0x7fffffff;
+            UInt32 theIntTwo = 0xffffffff;
+            
+            Console.WriteLine($"theIntOne: {theIntOne} (hex: {theIntOne:x8})");
+            Console.WriteLine($"theIntTwo: {theIntTwo} (hex: {theIntTwo:x8})\n");
+
+            theIntOne -= 147483647;
+            theIntTwo -= 294967295;
+            
+            Console.WriteLine($"theIntOne: {theIntOne} (hex: {theIntOne:x8})");
+            Console.WriteLine($"theIntTwo: {theIntTwo} (hex: {theIntTwo:x8})\n");
+
+            theIntOne = 0x7fffffff;
+            theIntTwo = 0xffffffff;
+            
+            Console.WriteLine($"theIntOne: {theIntOne} (hex: {theIntOne:x8})");
+            Console.WriteLine($"theIntTwo: {theIntTwo} (hex: {theIntTwo:x8})\n");
+            
+            /* -- These will overflow -- */
+
+            theIntOne++;
+            theIntTwo++;
+            
+            Console.WriteLine($"theIntOne: {theIntOne} (hex: {theIntOne:x8})");
+            Console.WriteLine($"theIntTwo: {theIntTwo} (hex: {theIntTwo:x8})\n");
+
+            theIntOne = -0x7fffffff - 1;
+            theIntTwo = 0;
+            
+            Console.WriteLine($"theIntOne: {theIntOne} (hex: {theIntOne:x8})");
+            Console.WriteLine($"theIntTwo: {theIntTwo} (hex: {theIntTwo:x8})\n");
+            
+            /* -- These will underflow -- */
+
+            theIntOne--;
+            theIntTwo--;
+            
+            Console.WriteLine($"theIntOne: {theIntOne} (hex: {theIntOne:x8})");
+            Console.WriteLine($"theIntTwo: {theIntTwo} (hex: {theIntTwo:x8})\n");
+        }
+    }
+}
