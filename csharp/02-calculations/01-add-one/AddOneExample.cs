6bf1d14574c82e63b362666114a62556b8db7ccd
Cole Andress <coleandress@gmail.com>
2020-12-12 11:35:39 -0600

    Added C# add one example and changed folder name for C.

---
+++ csharp/02-calculations/01-add-one/AddOneExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class AddOneExample
+    {
+        public static void Main(string[] args)
+        {
+            var someNumber = 42;
+            var ten        = 10;
+
+            Console.WriteLine($"someNumber: {someNumber}");
+            
+            /* -- Add one in the most vanilla way -- */
+
+            someNumber = someNumber + 1;
+
+            Console.WriteLine($"someNumber: {someNumber}");
+            
+            /* -- Add one in a moderately slicker way -- */
+
+            someNumber += 1;
+            
+            Console.WriteLine($"someNumber: {someNumber}");
+            
+            /* -- Add one in the most succinct way -- */
+
+            someNumber++;
+            
+            Console.WriteLine($"someNumber: {someNumber}\n");
+
+            Console.WriteLine($"ten: {ten}");
+            Console.WriteLine($"ten++: {ten++}");
+            Console.WriteLine($"++ten: {++ten}");
+            Console.WriteLine($"ten: {ten}");
+        }
+    }
+}
