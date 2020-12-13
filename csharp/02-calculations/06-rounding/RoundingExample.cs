2808a915e24fa9d9d4d962f37735ea5173febb23
Cole Andress <coleandress@gmail.com>
2020-12-13 14:22:38 -0600

    Added C# rounding example.

---
+++ csharp/02-calculations/06-rounding/RoundingExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class RoundingExample
+    {
+        public static void Main(string[] args)
+        {
+            var valueOne   = 1.4f;
+            var valueTwo   = 1.5f;
+            var valueThree = 1.6f;
+
+            var valueFour = -1.4f;
+            var valueFive = -1.5f;
+            var valueSix  = -1.6f;
+
+            Console.WriteLine($"{valueOne} => {Math.Round(valueOne)}");
+            Console.WriteLine($"{valueTwo} => {Math.Round(valueTwo)}");
+            Console.WriteLine($"{valueThree} => {Math.Round(valueThree)}");
+            Console.WriteLine($"{valueFour} => {Math.Round(valueFour)}");
+            Console.WriteLine($"{valueFive} => {Math.Round(valueFive)}");
+            Console.WriteLine($"{valueSix} => {Math.Round(valueSix)}");
+        }
+    }
+}
