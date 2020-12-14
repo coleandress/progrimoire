c71ce31f1e7729e20d640f5eaa20f15ea75a0c31
Cole Andress <coleandress@gmail.com>
2020-12-14 13:49:51 -0600

    Added C# math example.

---
+++ csharp/02-calculations/09-math/MathExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class MathExample
+    {
+        public static void Main(string[] args)
+        {
+            const int value = -12345;
+
+            Console.WriteLine($"The square root of: 64.0 is: {Math.Sqrt(64.0)}");
+            Console.WriteLine($"The sin of: {Math.PI} is: {Math.Sin(Math.PI)}");
+            Console.WriteLine($"The cos of: {Math.PI} is: {Math.Cos(Math.PI)}");
+            Console.WriteLine($"The natural log of: 2.0 is: {Math.Log(2.0)}");
+            Console.WriteLine($"The log base 10 of: 1000.0 is: {Math.Log(1000.0)}");
+            Console.WriteLine($"The value of: exp(1.0) is: {Math.Exp(1.0)}");
+            Console.WriteLine($"The value of: 2.0^3.0 is: {Math.Pow(2.0, 3.0)}");
+            Console.WriteLine($"The absolute value of: {value} is: {Math.Abs(value)}");
+        }
+    }
+}
