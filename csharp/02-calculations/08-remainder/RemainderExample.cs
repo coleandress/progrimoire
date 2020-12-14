1c9fefcb04a9b9584d1481061e636a5c96e0c55f
Cole Andress <coleandress@gmail.com>
2020-12-14 13:39:14 -0600

    Added C# remainder example.

---
+++ csharp/02-calculations/08-remainder/RemainderExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class RemainderExample
+    {
+        public static void Main(string[] args)
+        {
+            var ten = 10;
+
+            Console.WriteLine($"Remainder when {ten} is divided by 1 is: {ten % 1}");
+            Console.WriteLine($"Remainder when {ten} is divided by 2 is: {ten % 2}");
+            Console.WriteLine($"Remainder when {ten} is divided by 3 is: {ten % 3}");
+            Console.WriteLine($"Remainder when {ten} is divided by 4 is: {ten % 4}");
+            Console.WriteLine($"Remainder when {ten} is divided by 5 is: {ten % 5}");
+            Console.WriteLine($"Remainder when {ten} is divided by 6 is: {ten % 6}");
+            Console.WriteLine($"Remainder when {ten} is divided by 7 is: {ten % 7}");
+            Console.WriteLine($"Remainder when {ten} is divided by 8 is: {ten % 8}");
+            Console.WriteLine($"Remainder when {ten} is divided by 9 is: {ten % 9}");
+            Console.WriteLine($"Remainder when {ten} is divided by 10 is: {ten % 10}");
+        }
+    }
+}
