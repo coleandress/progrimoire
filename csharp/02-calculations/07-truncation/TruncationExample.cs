ec155ba82e72975ba2589a95d340ed7fa6e78960
Cole Andress <coleandress@gmail.com>
2020-12-14 13:33:34 -0600

    Added C# truncation example.

---
+++ csharp/02-calculations/07-truncation/TruncationExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class TruncationExample
+    {
+        public static void Main(string[] args)
+        {
+            var valueOne   = 1.4f;
+            var valueTwo   = 1.5f;
+            var valueThree = 1.9f;
+
+            var valueFour = -1.4f;
+            var valueFive = -1.5f;
+            var valueSix  = -1.9f;
+
+            Console.WriteLine($"{valueOne} => {(int)valueOne}");
+            Console.WriteLine($"{valueTwo} => {(int)valueTwo}");
+            Console.WriteLine($"{valueThree} => {(int)valueThree}");
+            Console.WriteLine($"{valueFour} => {(int)valueFour}");
+            Console.WriteLine($"{valueFive} => {(int)valueFive}");
+            Console.WriteLine($"{valueSix} => {(int)valueSix}");
+        }
+    }
+}
