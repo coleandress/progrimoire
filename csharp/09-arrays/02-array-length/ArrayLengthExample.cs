05de29cb406544551cc63d10e1468fb8f46f0487
Cole Andress <coleandress@gmail.com>
2020-12-31 12:48:55 -0600

    Added C# array length example.

---
+++ csharp/09-arrays/02-array-length/ArrayLengthExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class ArrayLengthExample
+    {
+        public static void Main(string[] args)
+        {
+            int[] someArray = { 2, 3, 5, 7, 11 };
+
+            for (var i = 0; i < someArray.Length; i++)  // C# gives arrays a Length property
+            {
+                Console.WriteLine($"someArray[{i}] = {someArray[i]}");
+            }
+        }
+    }
+}
