093adcb08078b6da53c3b6f04349a6fba1e9d5aa
Cole Andress <coleandress@gmail.com>
2020-12-31 13:17:52 -0600

    Added C# array step through elements example.

---
+++ csharp/09-arrays/04-array-step-through-elements/ArrayStepThroughElementsExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class ArrayStepThroughElementsExample
+    {
+        public static void Main(string[] args)
+        {
+            /* -- Create and print our initial array -- */
+            
+            int[] someArray = { 2, 3, 5, 7, 11 };
+
+            for (var i = 0; i < someArray.Length; i++)
+            {
+                Console.WriteLine($"someArray[{i}] = {someArray[i]}");
+            }
+
+            Console.WriteLine();
+            
+            /* -- Now print it backwards -- */
+
+            for (var i = someArray.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"someArray[{i}] = {someArray[i]}");
+            }
+
+            Console.WriteLine();
+            
+            /* -- Now, print every other element -- */
+
+            for (var i = 0; i < someArray.Length; i += 2)
+            {
+                Console.WriteLine($"someArray[{i}] = {someArray[i]}");
+            }
+        }
+    }
+}
