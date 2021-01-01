6c097c36d26239fe0dd806faa78bfe3e71bba9c8
Cole Andress <coleandress@gmail.com>
2021-01-01 09:51:18 -0600

    Added C# add items to array example.

---
+++ csharp/09-arrays/06-add-items-to-array/AddItemsToArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class AddItemsToArrayExample
+    {
+        private const int Items = 20;
+        private const int N = 6;
+        
+        public static void Main(string[] args)
+        {
+            /* -- Initialize array and resize -- */
+            
+            var someArray = new int[] { 2, 3, 5, 7, 11, 13 };
+            Array.Resize(ref someArray, Items);
+            
+            /* -- Now let's store elements in the unused locations --*/
+
+            for (int i = N, j = -1; i < Items; i++, j--)
+                someArray[i] = j;
+            
+            for (int i = 0, n = someArray.Length; i < n; i++)
+                Console.WriteLine($"someArray[{i}] = {someArray[i]}");
+
+            Console.WriteLine();
+        }
+    }
+}
