add1992943f76b89046eff14e1190b355b4d4930
Cole Andress <coleandress@gmail.com>
2021-01-03 12:45:40 -0600

    Added C# array count occurrences example.

---
+++ csharp/09-arrays/11-count-occurrences/CountOccurrencesExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class CountOccurrencesExample
+    {
+        public static void Main(string[] args)
+        {
+            var someArray = new int[] { 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 };
+
+            /* -- Using the Array.FindAll() method -- */
+            
+            for (var i = 0; i < 6; i++)
+            {
+                var foundItems = Array.FindAll(someArray, (num) => num == i);
+                Console.WriteLine("{0} was found {1} time{2}", i, foundItems.Length, foundItems.Length == 1 ? "" : "s");
+            }
+
+            Console.WriteLine();
+            
+            /* -- Without making use of the Array.FindAll() method -- */
+
+            for (var i = 0; i < 6; i++)
+            {
+                var count = 0;
+
+                foreach (var num in someArray)
+                    if (num == i)
+                        count++;
+
+                Console.WriteLine("{0} was found {1} time{2}", i, count, count == 1 ? "" : "s");
+            }
+
+            Console.WriteLine();
+            
+            /* -- Using LINQ -- */
+
+            for (var i = 0; i < 6; i++)
+            {
+                var count = someArray.Count(num => num == i);
+
+                Console.WriteLine("{0} was found {1} time{2}", i, count, count == 1 ? "" : "s");
+            }
+        }
+    }
+}
