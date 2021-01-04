b3714b11a7299fbb780ba5ed6075cee1a6db2d23
Cole Andress <coleandress@gmail.com>
2021-01-04 15:38:14 -0600

    Added C# read float example.

---
+++ csharp/03-input/03-read-float/ReadFloatExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class ReadFloatExample
+    {
+        public static void Main(string[] args)
+        {
+            /* -- This works the same for double or decimal -- */
+            
+            Console.Write("Enter a float: ");
+            string theStringInput = Console.ReadLine();
+
+            bool isValidInput = float.TryParse(theStringInput, out float theFloat);
+
+            if (isValidInput)
+                Console.WriteLine($"You entered: {theFloat}");
+            else
+                Console.WriteLine($"error: invalid input [{theStringInput}]");
+        }
+    }
+}
