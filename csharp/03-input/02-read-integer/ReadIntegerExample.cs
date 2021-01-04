ddcc2a067efdc822973caa1cc98965c998c889e7
Cole Andress <coleandress@gmail.com>
2021-01-04 15:30:34 -0600

    Added C# read integer example.

---
+++ csharp/03-input/02-read-integer/ReadIntegerExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class ReadIntegerExample
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            string theStringInput = Console.ReadLine();
+
+            bool isValidInput = int.TryParse(theStringInput, out int theInteger);
+
+            if (isValidInput)
+                Console.WriteLine($"You entered: {theInteger}");
+            else
+                Console.WriteLine($"error: invalid input [{theStringInput}]");
+        }
+    }
+}
