729137b49b403096dca1913f6e4290f93f08b4a6
Cole Andress <coleandress@gmail.com>
2021-01-04 15:20:18 -0600

    Added C# ReadLine() example.

---
+++ csharp/03-input/01-read-line/ReadLineExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class ReadLineExample
+    {
+        public static void Main(string[] args)
+        {
+            var theLines = new List<string>();
+            
+            while (true)
+            {
+                Console.WriteLine("Enter some text (or 'exit' to end): ");
+
+                string theInput = Console.ReadLine();
+
+                if (theInput == "exit")
+                    break;
+                
+                theLines.Add(theInput);
+            }
+
+            Console.WriteLine(string.Join("\n", theLines));
+            
+            /* -- Keep program running until a key is pressed -- */
+
+            Console.WriteLine("Press End key to exit.");
+
+            while (Console.ReadKey().Key != ConsoleKey.End) { }
+        }
+    }
+}
