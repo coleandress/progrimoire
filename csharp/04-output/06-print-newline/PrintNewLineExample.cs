eeaa861eb5a2fa07bc79ec9d4168b6a6400b7330
Cole Andress <coleandress@gmail.com>
2021-01-16 12:21:07 -0600

    Added C# print new line example.

---
+++ csharp/04-output/06-print-newline/PrintNewLineExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class PrintNewlineExample
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("H");
+            Console.WriteLine("e");
+            Console.WriteLine("l");
+            Console.WriteLine("l");
+            Console.WriteLine("o");
+        }
+    }
+}
