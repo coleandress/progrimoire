9d32b52ece8b82422fd7d464c25ff94820e4b566
Cole Andress <coleandress@gmail.com>
2021-01-17 11:13:44 -0600

    Added C# function no params no return example.

---
+++ csharp/05-functions/01-no-params-no-return/NoParamsNoReturnExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class NoParamsNoReturnExample
+    {
+        public static void Main(string[] args)
+        {
+            Greet();
+        }
+
+        /* -- This function takes no arguments
+              and does not return anything -- */
+        
+        private static void Greet()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
