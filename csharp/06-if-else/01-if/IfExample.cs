5c57fa9e6c842f8872aa0dcdcd230231c946cb12
Cole Andress <coleandress@gmail.com>
2021-01-21 15:20:10 -0600

    Added C# if example.

---
+++ csharp/06-if-else/01-if/IfExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class IfExample
+    {
+        public static void Main(string[] args)
+        {
+            var x = 42;
+            
+            /* -- All these conditions are true so all the
+                  'WriteLine()' lines will be executed -- */
+            
+            if (x > 10)
+                Console.WriteLine("x was more than ten ...");
+            
+            if (x == 42)
+                Console.WriteLine("x was forty two ...");
+            
+            if (x < 50)
+                Console.WriteLine("x was less than fifty ...\n");
+        }
+    }
+}
