18509183ed838986d9f9cd5aa8b0ca8e9bec2a5d
Cole Andress <coleandress@gmail.com>
2021-01-21 15:51:03 -0600

    Added C# if else chain example.

---
+++ csharp/06-if-else/04-if-else-chain/IfElseChainExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class IfElseChainExample
+    {
+        public static void Main(string[] args)
+        {
+            var x = 42;
+            
+            /* -- Either: x is less than 42, x is greater than 42 or
+                  x equals 42 - three mutually exclusive alternatives -- */
+            
+            if (x < 42)
+                Console.WriteLine("x is less than forty two ...\n");
+            
+            else if (x > 42)
+                Console.WriteLine("x is greater than forty two ... \n");
+
+            else
+                Console.WriteLine("x equals forty two ... \n");
+            
+            /* -- Blank lines not required between if-else blocks, but
+                  added for clarity.  Use or don't use based on 
+                  personal preference. -- */
+        }
+    }
+}
