69e4a0c833694a12943e7d9ce084ef815c78e12d
Cole Andress <coleandress@gmail.com>
2021-01-21 15:58:46 -0600

    Added C# ternary example.

---
+++ csharp/06-if-else/05-ternary/TernaryExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class TernaryExample
+    {
+        public static void Main(string[] args)
+        {
+            var x = 42;
+            
+            /* -- The ternary operator is essentially an in-line, shorthand
+                  for an if/else statement.  It evaluates to the first term
+                  if the test is true or the second term if it is not -- */
+
+            Console.WriteLine("x is {0} ...\n", x % 2 == 0 ? "even" : "odd");
+
+            var y = 24;
+            
+            /* -- Parens added for clarity, not required -- */
+
+            var max = (x >= y) ? x : y;
+
+            Console.WriteLine($"max: {max}\n");
+        }
+    }
+}
