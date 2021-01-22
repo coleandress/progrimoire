2b5c2573eb87ca737d52e8ffde4657fd238be195
Cole Andress <coleandress@gmail.com>
2021-01-22 15:00:57 -0600

    Added C# count down ten to one example.

---
+++ csharp/08-loops/04-count-down-ten-to-one/CountDownTenToOneExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class CountDownTenToOneExample
+    {
+        public static void Main(string[] args)
+        {
+            /* -- Count down starting from ten to one using a while,
+                  do-while and for loop -- */
+
+            var i = 10;
+
+            while (i >= 1)
+                Console.WriteLine($"i: {i--}");
+
+            Console.WriteLine();
+
+            i = 10;
+
+            do
+            {
+                Console.WriteLine($"i: {i}");
+                i -= 1;
+                
+            } while (i >= 1);
+            
+            Console.WriteLine();
+            
+            for (i = 10; i > 0; i--)
+                Console.WriteLine($"i: {i}");
+
+            Console.WriteLine();
+        }
+    }
+}
