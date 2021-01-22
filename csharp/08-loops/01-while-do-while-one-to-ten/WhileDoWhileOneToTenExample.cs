2d88db7d1d9c39b21a4a0b7d7aeebbb4c87b59d2
Cole Andress <coleandress@gmail.com>
2021-01-22 14:31:55 -0600

    Added C# while do-while one to ten example.

---
+++ csharp/08-loops/01-while-do-while-one-to-ten/WhileDoWhileOneToTenExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class WhileDoWhileOneToTenExample
+    {
+        public static void Main(string[] args)
+        {
+            /* -- Count from one to ten using both a while
+                  loop and a do-while loop -- */
+
+            var i = 0;
+            
+            while (i <= 10)
+                Console.WriteLine($"i: {i++}");
+
+            Console.WriteLine();
+
+            i = 1;
+
+            do
+            {
+                Console.WriteLine($"i: {i++}");
+                
+            } while (i <= 10);
+        }
+    }
+}
