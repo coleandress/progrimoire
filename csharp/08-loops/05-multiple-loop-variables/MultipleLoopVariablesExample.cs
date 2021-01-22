3a4931fac68f808f3177082eaa0e40dea91bf21c
Cole Andress <coleandress@gmail.com>
2021-01-22 15:10:16 -0600

    Added C# multiple loop variables example.

---
+++ csharp/08-loops/05-multiple-loop-variables/MultipleLoopVariablesExample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class MultipleLoopVariablesExample
+    {
+        public static void Main(string[] args)
+        {
+            /* -- First, here's a for loop that uses multiple loop variables -- */
+            
+            for (int i = 0, j = 1, k = 10; i < 10; i++, j += 2, k--)
+                Console.WriteLine($"i: {i}, j: {j}, k: {k}");
+
+            Console.WriteLine();
+            
+            /* -- Now, a while loop example that uses multiple
+                  loop variables -- */
+
+            var m = 2;
+            var n = 3;
+            var o = 5;
+
+            while (m + n + o < 50)
+            {
+                Console.WriteLine($"m: {m}, n: {n}, o: {o}");
+
+                m++;
+                n *= 2;
+                o += 3;
+            }
+
+            Console.WriteLine();
+            
+            /* -- And finally, a do-while loop example using multiple loop variables -- */
+
+            m = n = o = 0;
+
+            do
+            {
+                Console.WriteLine($"m: {m}, n: {n}, o: {o}");
+
+                m += 2;
+                n += 3;
+                o += 5;
+                
+            } while (m + n + o < 50);
+
+            Console.WriteLine();
+        }
+    }
+}
