95497f183c4f68e23c958de755ed5d11a30f40b3
Cole Andress <coleandress@gmail.com>
2021-01-22 20:22:49 -0600

    Added C# access some array elements example.

---
+++ csharp/08-loops/07-access-some-array-elements/AccessSomeArrayElementsExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class AccessSomeArrayElementsExample
+    {
+        public static void Main(string[] args)
+        {
+            var someArray = new int[] {2, 3, 5, 7, 11, 13};
+            
+            /* -- Show every other element -- */
+            
+            for (var i = 0; i < someArray.Length; i += 2)
+                Console.WriteLine($"someArray[{i}] = {someArray[i]}");
+
+            Console.WriteLine();
+            
+            /* -- Show the first half of the array -- */
+            
+            for (var i = 0; i < someArray.Length / 2; i++)
+                Console.WriteLine($"someArray[{i}] = {someArray[i]}");
+
+            Console.WriteLine();
+            
+            /* -- Now the last half of the array -- */
+            
+            for (var i = someArray.Length / 2; i < someArray.Length; i++)
+                Console.WriteLine($"someArray[{i}] = {someArray[i]}");
+
+            Console.WriteLine();
+        }
+    }
+}
