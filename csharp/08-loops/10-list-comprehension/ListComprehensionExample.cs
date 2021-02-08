9c46af0d1f743910381519b8825d698c02480bd5
Cole Andress <coleandress@gmail.com>
2021-02-08 14:20:39 -0600

    Added C# List Comprehension Example.

---
+++ csharp/08-loops/10-list-comprehension/ListComprehensionExample.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class ListComprehensionExample
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            
+            for (var i = 0; i < 100000; i++)
+                numbers.Add(i);
+
+            /* -- Find all the primes in a list of numbers using Linq -- */
+            
+            var primes = numbers.Where(IsPrime).Select(x => x).ToList();
+            
+            foreach (var num in primes)
+                Console.WriteLine($"{num}");
+
+            Console.WriteLine();
+            
+            /* -- Double all numbers from 0 to 100 -- */
+
+            var doubledNumbers = from x in Enumerable.Range(0, 101)
+                select x * 2;
+            
+            foreach (var num in doubledNumbers)
+                Console.WriteLine($"{num}");
+            
+            /* -- Square all numbers in the numbers list that are less than 50 -- */
+
+            var squaredNumbers = from x in numbers
+                where x < 50
+                select x * x;
+            
+            foreach (var num in squaredNumbers)
+                Console.WriteLine($"{num}");
+        }
+
+        private static bool IsPrime(int num)
+        {
+            if (num < 2)
+                return false;
+            
+            for (var i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
