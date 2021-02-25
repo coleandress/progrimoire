257196ad16c0bc563f6cc975e507f5e3dbb57444
Cole Andress <coleandress@gmail.com>
2021-02-24 19:48:13 -0600

    Added C# sum min max avg example.

---
+++ csharp/10-2d-arrays/06-sum-min-max-avg/SumMinMaxAvgExample.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class SumMinMaxAvgExample
+    {
+        public static void Main(string[] args)
+        {
+            var someArray = new[]
+            {
+                new[] {  2,  3,  5,  7, 11 },
+                new[] { 13, 17, 19, 23, 29 },
+                new[] { 31, 37, 41, 43, 47 },
+                new[] { 53, 57, 59, 61, 67 }
+            };
+
+            var min = someArray.Min(x => x.Min());
+            var max = someArray.Max(x => x.Max());
+            var sum = someArray.Sum(x => x.Sum());
+
+            Console.WriteLine($"sum: {sum}");
+            Console.WriteLine($"min: {min}");
+            Console.WriteLine($"max: {max}");
+            Console.WriteLine("avg: {0:0.##}\n", (double)sum / (someArray.Length * someArray[0].Length));
+        }
+    }
+}
