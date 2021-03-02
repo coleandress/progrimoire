92da617b7a04c749c3a381e519d4a6e8868cd2d9
Cole Andress <coleandress@gmail.com>
2021-03-02 17:59:20 -0600

    Added C# count occurrences example.

---
+++ csharp/10-2d-arrays/09-count-occurrences/CountOccurrencesExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class CountOccurrencesExample
+    {
+        public static void Main(string[] args)
+        {
+            var someArray = new int[,]
+            {
+                {  1,  2,  2,  3,  3 },
+                {  3,  4,  4,  4,  4 },
+                {  5,  5,  5,  5,  5 },
+                {  1,  2,  3,  4,  5 }
+            };
+
+            for (var number = 2; number < 6; number++)
+            {
+                var count = 0;
+                
+                for (var i = 0; i < someArray.GetLength(0); i++)
+                    for (var j = 0; j < someArray.GetLength(1); j++)
+                        if (someArray[i, j] == number)
+                            count++;
+
+                Console.WriteLine($"number: {number}, count: {count}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
