a53007df8875a0c7d28c0336a94c9cf968fc0984
Cole Andress <coleandress@gmail.com>
2021-03-03 00:13:25 -0600

    Added C# declare growable array (List) example.

---
+++ csharp/11-growable-arrays/01-declare-growable-array/DeclareGrowableArrayExample.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class DeclareGrowableArrayExample
+    {
+        public static void Main(string[] args)
+        {
+            /* -- Initialize the List and fill it with five 0s -- */
+            
+            var iList = new List<int>();
+
+            PrintList(iList);
+
+            for (var i = 0; i < 5; i++)
+                iList.Add(0);
+            
+            PrintList(iList);
+
+            /* -- Add primes up to 49 to the List -- */
+            
+            for (var i = 2; i < 50; i++)
+            {
+                var divisors = 0;
+                
+                for (var j = 2; j < i; j++)
+                    if (i % j == 0)
+                        divisors++;
+
+                if (divisors == 0)
+                    iList.Add(i);
+            }
+            
+            PrintList(iList);
+
+            /* -- Remove five elements starting at index 0 -- */
+            
+            iList.RemoveRange(0, 5);
+            
+            PrintList(iList);
+
+            Console.WriteLine();
+        }
+
+        private static void PrintList(List<int> list)
+        {
+            Console.Write("{ ");
+            
+            for(var i = 0; i < list.Count; i++)
+                Console.Write("{0}{1}", i == 0 ? "" : ", ", list[i]);
+            
+            Console.Write(" }\n");
+        }
+    }
+}
