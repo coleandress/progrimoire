5db82613f98ede574ec92bbe3b66e907a7957135
Cole Andress <coleandress@gmail.com>
2021-03-03 13:57:28 -0600

    Added C# growable array (List) step through elements example.

---
+++ csharp/11-growable-arrays/04-growable-array-step-through-elements/GrowableArrayStepThroughElementsExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class GrowableArrayStepThroughElementsExample
+    {
+        public static void Main(string[] args)
+        {
+            var iList = new List<int>();
+
+            /* -- Add primes up to 49 to the List -- */
+            
+            for (var i = 2; i < 50; i++)
+            {
+                var divisors = 0;
+                
+                for (var j = 2; j < i; j++)
+                    if (i % j == 0)
+                        divisors++;
+
+                if (divisors == 0)
+                    iList.Add(i);
+            }
+            
+            /* -- Step through elements with foreach loop -- */
+            
+            foreach (var num in iList)
+                Console.WriteLine(num);
+
+            Console.WriteLine();
+            
+            /* -- foreach with indexes assuming no duplicates -- */
+            
+            foreach (var num in iList)
+                Console.WriteLine($"iList[{iList.IndexOf(num)}] = {num}");
+
+            Console.WriteLine();
+            
+            /* -- for loop -- */
+
+            for (var i = 0; i < iList.Count; i++)
+                Console.WriteLine($"iList[{i}] = {iList[i]}");
+        }
+    }
+}
