49db73d44ada3f85e92933eb72cf9c5e6f0af490
Cole Andress <coleandress@gmail.com>
2021-03-13 14:02:56 -0600

    Added C# remove whitespace example.

---
+++ csharp/12-strings/02-remove-whitespace/RemoveWhitespaceExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class RemoteWhitespaceExample
+    {
+        public static void Main(string[] args)
+        {
+            var aString = "There is a bit of whitespace in this string";
+            var bString = "ThisOneOnTheOtherHandDoesn'tHaveAny";
+
+            var aStringNoWhitespace = aString.Replace(" ", "");
+            var bStringNoWhitespace = bString.Replace(" ", "");
+            
+            Console.WriteLine($"{aStringNoWhitespace}");
+            Console.WriteLine($"{bStringNoWhitespace}");
+        }
+    }
+}
