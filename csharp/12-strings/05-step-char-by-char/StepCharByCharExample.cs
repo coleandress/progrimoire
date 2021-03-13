d9a6bc640727a47b5d29e36b202658605c1b1fe0
Cole Andress <coleandress@gmail.com>
2021-03-13 15:06:12 -0600

    Added C# step char by char example.

---
+++ csharp/12-strings/05-step-char-by-char/StepCharByCharExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class StepCharByCharExample
+    {
+        public static void Main(string[] args)
+        {
+            const string s = "pack my box with five dozen liquor jugs";
+            
+            for (var i = 0; i < s.Length; i++)
+                Console.WriteLine($"s[{i}] = '{s[i]}'");
+
+            Console.WriteLine();
+
+            foreach (var c in s)
+                Console.WriteLine($"{(char) (c + 5)}");
+        }
+    }
+}
