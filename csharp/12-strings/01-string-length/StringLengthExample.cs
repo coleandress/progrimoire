1535f4775c17d3158a1de190473b2a870de01955
Cole Andress <coleandress@gmail.com>
2021-03-13 13:54:33 -0600

    Added C# string length example.

---
+++ csharp/12-strings/01-string-length/StringLengthExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class StringLengthExample
+    {
+        public static void Main(string[] args)
+        {
+            var theString = "abcdefghijklmnopqrstuvwxyz";
+
+            /* -- String objects have a Length property -- */
+            
+            Console.WriteLine($"length of: '{theString}' is {theString.Length}");
+        }
+    }
+}
