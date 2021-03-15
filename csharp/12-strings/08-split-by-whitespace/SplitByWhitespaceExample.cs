4145a315e8d52c32db5d11c9bf454100d23cab97
Cole Andress <coleandress@gmail.com>
2021-03-15 15:03:44 -0500

    Added C# split by whitespace example.

---
+++ csharp/12-strings/08-split-by-whitespace/SplitByWhitespaceExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class SplitByWhitespaceExample
+    {
+        public static void Main(string[] args)
+        {
+            const string s = "a string with words separated by whitespace";
+            Console.WriteLine(s);
+
+            Console.WriteLine();
+            
+            var splitString = s.Split(' ');
+            
+            foreach (var word in splitString)
+                Console.WriteLine(word);
+        }
+    }
+}
