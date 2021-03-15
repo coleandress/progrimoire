fdad3e8efd2e2a5555c064208342552b1581a12b
Cole Andress <coleandress@gmail.com>
2021-03-15 14:56:59 -0500

    Added C# extract substring example.

---
+++ csharp/12-strings/07-extract-substring/ExtractSubstringExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class ExtractSubstringExample
+    {
+        public static void Main(string[] args)
+        {
+            const string theString = "abcdefghijklmnopqrstuvwxyz";
+            
+            for (var i = 1; i < 8; i++)
+                Console.WriteLine($"'{theString.Substring(i, 2 * i)}'");
+        }
+    }
+}
