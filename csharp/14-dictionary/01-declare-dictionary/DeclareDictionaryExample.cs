085b67856d9acdac25cdab1040f8609d9e2647da
Cole Andress <coleandress@gmail.com>
2021-03-15 15:12:11 -0500

    Added C# declare dictionary example.

---
+++ csharp/14-dictionary/01-declare-dictionary/DeclareDictionaryExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrimoireCSharpExamples
+{
+    internal static class DeclareDictionaryExample
+    {
+        public static void Main(string[] args)
+        {
+            var inventory = new Dictionary<string, int>();
+            
+            inventory.Add("Sword", 1);
+            inventory.Add("Shield", 1);
+            inventory.Add("Potions", 10);
+            
+            foreach (var i in inventory)
+                Console.WriteLine($"{i.Key, 8}: {i.Value, 2}");
+        }
+    }
+}
